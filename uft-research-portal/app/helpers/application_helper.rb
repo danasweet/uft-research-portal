2a314394d56a3ef66044af3ec103dc9b32a8a040
shebryant <shebryant@gmail.com>
2015-10-26 18:43:57 -0500

    Create blank view for edit.' git checkout master '

--- uft-research-portal/app/helpers/application_helper.rb
+++ uft-research-portal/app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
-  def logged_in?
-    session[:user_id] ? true : false
-  end
-
-  def current_user
-    @user || User.find_by(id: session[:user_id]) if logged_in?
-  end
-
+  
 end
