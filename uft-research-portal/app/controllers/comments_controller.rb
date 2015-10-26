966892af9ecd76333d809c4f37bf5c0ce48414e5
Dennis <email": null>
2015-10-26 18:07:55 -0500

    Added comments to proposals

---
+++ uft-research-portal/app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  respond_to :html, :js
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    form_params = params["comment"]
+    @proposal = Proposal.find(params[:proposal_id])
+    @experiment = @proposal.experiments.find_by(id: params[:experiment_id])
+    @comment = Comment.create(content: form_params[:content], stage: form_params[:stage], commenter_id: session[:user_id], experiment_id: params[:experiment_id], proposal_id: params[:proposal_id])
+    if @comment.valid? && request.xhr?
+      if @comment.experiment_id
+        @experiment.comments << @comment
+      else
+        @proposal.comments << @comment
+      end
+    end
+  end
+
+end
