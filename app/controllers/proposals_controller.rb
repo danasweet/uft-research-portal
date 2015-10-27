66766edd0e9193349fd1765405560d909eecc3d9
danasweet <dsweet100@gmail.com>
2015-10-27 11:55:11 -0500

    add initial authorizations, helper methods

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,18 +1,21 @@
 class ProposalsController < ApplicationController
 
+  before_filter :authorize
+  # before_filter :is_faculty?, :except => [:index, :show]
+
   def index
     @proposals = Proposal.all
   end
 
-  def new 
+  def new
     @proposal = Proposal.new
   end
 
-  def create 
+  def create
     @proposal = Proposal.create!(proposal_params)
     # @proposal.faculty = current_user
     @proposal.faculty = User.find_by(id: session[:user_id])
-    @proposal.save 
+    @proposal.save
       if @proposal.valid?
         redirect_to proposal_path(@proposal)
       else
