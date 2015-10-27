4c260c7d1b9ab55fd11049a5e22ebdc99c96809f
Dennis <email": null>
2015-10-27 11:50:55 -0500

    Fixed comment adding

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
   def create
     form_params = params["comment"]
     @proposal = Proposal.find(params[:proposal_id])
-    @experiment = @proposal.experiments.find_by(id: params[:experiment_id])
+    @experiment = Experiment.find_by(id: params[:experiment_id])
     @comment = Comment.create(content: form_params[:content], stage: params[:stage], commenter_id: session[:user_id], experiment_id: params[:experiment_id], proposal_id: params[:proposal_id])
     if @comment.valid? && request.xhr?
       if @comment.experiment_id
